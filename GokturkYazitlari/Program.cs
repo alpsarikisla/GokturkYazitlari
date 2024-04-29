a842b940e585b83fa4a830924a0dcdd2eeed6378
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2024-04-29 14:15:23 +0300

    OneriVeDuzenlemeler

--- GokturkYazitlari/Program.cs
+++ GokturkYazitlari/Program.cs
@@ -15,12 +15,12 @@ namespace GokturkYazitlari
             Console.WriteLine("\n Lütfen aramak istediğiniz Orhun Yazıtının İsmini Giriniz");
 
             string girilen_Metin = Console.ReadLine();
-
+            Urun[] urunler = new Urun[3];
             Urun u = new Urun();
             u.urunno = "1";
             u.isim = "Kül Tigin Yazıtı";
             u.yazar = "Yollug Tigin";
-            u.basımyılı = "552-744 yılları arasında";
+            u.basimYili = "552-744 yılları arasında";
             u.dil = "Orhun alfabesi";
             u.konum = " Moğolistan’da Orhun Vadisi'nde";
 
@@ -28,7 +28,7 @@ namespace GokturkYazitlari
             u1.urunno = "2";
             u1.isim = "Bilge Kağan Yazıtı";
             u1.yazar = "Yollug Tigin";
-            u1.basımyılı = "552-744 yılları arasında";
+            u1.basimYili = "552-744 yılları arasında";
             u1.dil = "Orhun alfabesi";
             u1.konum = " Moğolistan’da Orhun Vadisi'nde";
 
@@ -36,26 +36,27 @@ namespace GokturkYazitlari
             u2.urunno = "3";
             u2.isim = "Tonyukuk Yazısı";
             u2.yazar = "Tonyukuk";
-            u2.basımyılı = "552-744 yılları arasında";
+            u2.basimYili = "552-744 yılları arasında";
             u2.dil = "Orhun alfabesi";
             u2.konum = " Moğolistan’da Orhun Vadisi'nde";
 
-            Urun[] urunler = new Urun[3];
+           
             urunler[0] = u;
             urunler[1] = u1;
             urunler[2] = u2;
 
             if (girilen_Metin == "Kül Tigin Yazıtı")
             {
-                Console.WriteLine("Ürün nu: " + urunler[0].urunno + " " + " Yazıt İsmi: "+ urunler[0].isim + " "+ " Yazar: "+ urunler[0].yazar + " "+ " Basım Yılı: " + urunler[0].basımyılı+ " " + "Kullanılan dil: " + urunler[0].dil+ " " + "Yazıtın konumu: " + urunler[0].konum);
+                Urun uu = urunler[0];
+                Console.WriteLine("Ürün nu: " + uu.urunno + " " + " Yazıt İsmi: "+ uu.isim + " "+ " Yazar: "+ uu.yazar + " "+ " Basım Yılı: " + uu.basimYili + " " + "Kullanılan dil: " + uu.dil+ " " + "Yazıtın konumu: " + uu.konum);
             }
             else if (girilen_Metin == "Bilge Kağan Yazıtı")
             {
-                Console.WriteLine("Ürün nu: " + urunler[1].urunno + " " + " Yazıt İsmi: " + urunler[1].isim + " " + " Yazar: " + urunler[1].yazar + " " + " Basım Yılı: " + urunler[1].basımyılı + " " + "Kullanılan dil: " + urunler[1].dil + " " + "Yazıtın konumu: " + urunler[1].konum);
+                Console.WriteLine("Ürün nu: " + urunler[1].urunno + " " + " Yazıt İsmi: " + urunler[1].isim + " " + " Yazar: " + urunler[1].yazar + " " + " Basım Yılı: " + urunler[1].basimYili + " " + "Kullanılan dil: " + urunler[1].dil + " " + "Yazıtın konumu: " + urunler[1].konum);
             }
             else if (girilen_Metin == "Tonyukuk Yazıtı")
             {
-                Console.WriteLine("Ürün nu: " + urunler[2].urunno + " " + " Yazıt İsmi: " + urunler[2].isim + " " + " Yazar: " + urunler[2].yazar + " " + " Basım Yılı: " + urunler[2].basımyılı + " " + "Kullanılan dil: " + urunler[2].dil + " " + "Yazıtın konumu: " + urunler[2].konum);
+                Console.WriteLine("Ürün nu: " + urunler[2].urunno + " " + " Yazıt İsmi: " + urunler[2].isim + " " + " Yazar: " + urunler[2].yazar + " " + " Basım Yılı: " + urunler[2].basimYili + " " + "Kullanılan dil: " + urunler[2].dil + " " + "Yazıtın konumu: " + urunler[2].konum);
             }
             else 
             {
@@ -68,7 +69,7 @@ namespace GokturkYazitlari
         public string urunno;
         public string isim;
         public string yazar;
-        public string basımyılı;
+        public string basimYili;//Türkçe Karakter Kullanmamalısın
         public string dil;
         public string konum;
     }
